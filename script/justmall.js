143bb8ba56807c2a7ed436aee176d4f76f4121a9
L-Junho <wnsgh1255@naver.com>
2020-05-08 11:51:10 +0900

    팝업창 만들기

---
+++ script/justmall.js
@@ -0,0 +1,28 @@
+$(function(){        
+    tabs();  // tabs 기능 호출(=실행)
+    popup(); // popup 기능 호출
+});
+function tabs(){ // tabs 기능 정의
+    $(".board_title li").click(function(){
+        if(!$(this).hasClass("on")) {
+        $(this).siblings().removeClass("on");
+        $(this).addClass("on");
+        $("#gallery").toggleClass("hide");
+        }
+    });   
+}
+function popup(){ // popup 기능 정의   
+    popup_open();
+    popup_close();
+}
+function popup_open(){
+    $("#notice li:eq(0)").click(function(){ // 팝업창 열기 기능
+        $("#popup").css("display","block");
+    });
+}
+function popup_close(){
+    $("#popup input[type=button]").click(function(){
+        $(this).parent().css("display","none");
+        // 부모를 감추면 - 자식인 나도? 없어지겠죠~
+    })
+}
